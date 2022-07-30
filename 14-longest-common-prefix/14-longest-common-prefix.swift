e192a4eb9bf084475a2d2ba965409628e17f3d5b
Yoonseo Kim <72497599+ezidayzi@users.noreply.github.com>
2022-07-30 16:01:07 +0900

    Time: 148 ms (7.72%), Space: 14.4 MB (51.80%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.swift
+++ 14-longest-common-prefix/14-longest-common-prefix.swift
@@ -1,10 +1,17 @@
 class Solution {
     func longestCommonPrefix(_ strs: [String]) -> String {
-        guard var `prefix` = strs.min() else { return "" }
-        while `prefix`.isEmpty == false {
-            if strs.allSatisfy({ $0.hasPrefix(`prefix`) }) { break }
-                `prefix`.removeLast()
-            }
-            return `prefix`
+        guard let shortestString = strs.min() else {
+            return ""
         }
+        var longest = ""
+        for i in 0...shortestString.count {
+            let prefix = shortestString.prefix(i)
+            if strs.allSatisfy({ $0.hasPrefix(prefix)}) {
+                longest = String(prefix)
+            } else {
+                return longest
+            }
+        }
+        return longest
+    }
 }

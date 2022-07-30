0dbba6910d17bff5a4cd2a9dc28070a550fb47b4
Yoonseo Kim <72497599+ezidayzi@users.noreply.github.com>
2022-07-30 15:36:25 +0900

    Time: 70 ms (40.01%), Space: 14 MB (72.04%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.swift
+++ 9-palindrome-number/9-palindrome-number.swift
@@ -1,5 +1,16 @@
 class Solution {
     func isPalindrome(_ x: Int) -> Bool {
-        return String("\(x)".reversed()) == "\(x)"
+		if x < 0 { return false }
+		var nums: [Int] = []
+		var tmp = x
+        
+		while tmp > 0 {
+			nums.append(tmp % 10)
+			tmp = tmp / 10
+		}
+        
+		for (i, num) in nums.enumerated() where nums[nums.count - i - 1] != num { return false }
+        return true
     }
+
 }

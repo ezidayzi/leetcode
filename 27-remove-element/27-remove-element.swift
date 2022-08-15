ac3478df3b720d9fac53e67326620e3f267a47ba
Yoonseo Kim <72497599+ezidayzi@users.noreply.github.com>
2022-08-15 15:27:05 +0900

    Time: 7 ms (66.10%), Space: 13.9 MB (78.88%) - LeetHub

---
+++ 27-remove-element/27-remove-element.swift
@@ -0,0 +1,9 @@
+class Solution {
+    func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+        nums = nums.filter {
+            $0 != val
+        }
+
+        return nums.count
+    }
+}
